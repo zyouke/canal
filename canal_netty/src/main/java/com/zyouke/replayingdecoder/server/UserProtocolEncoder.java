22cf05c4c7e00aada79f28b7e7d63502dde5b73f
âzyouke <zyouke@aliyun.com>
2019-06-16 22:22:03 +0800

    netty 的编解码之ReplayingDecoder

---
+++ canal_netty/src/main/java/com/zyouke/replayingdecoder/server/UserProtocolEncoder.java
@@ -0,0 +1,18 @@
+package com.zyouke.replayingdecoder.server;
+
+import com.zyouke.replayingdecoder.protocol.UserProtocol;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.ReplayingDecoder;
+
+import java.util.List;
+
+public class UserProtocolEncoder extends MessageToByteEncoder<UserProtocol> {
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, UserProtocol msg, ByteBuf out) throws Exception{
+        out.writeInt(msg.getLength());
+        out.writeBytes(msg.getContent());
+    }
+}
