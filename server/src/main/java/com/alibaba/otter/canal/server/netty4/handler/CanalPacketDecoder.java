4a18ecc7a4abe493bbd1bc3a5eaa42862986790d
zhoujun <zhoujun@okay.cn>
2019-06-24 13:14:51 +0800

    调通canal项目

--- server/src/main/java/com/alibaba/otter/canal/server/netty4/handler/CanalPacketDecoder.java
+++ server/src/main/java/com/alibaba/otter/canal/server/netty4/handler/CanalPacketDecoder.java
@@ -1,5 +1,6 @@
 package com.alibaba.otter.canal.server.netty4.handler;
 
+import com.alibaba.otter.canal.protocol.CanalPacket;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ReplayingDecoder;
@@ -8,15 +9,18 @@ import java.util.List;
 
 
 /**
- * 解析对应的header信息
- * 
+ * CanalPacket 使用的协议是protocol
+ * 该类是将byteBuf解析成CanalPacket
  * @author jianghang 2012-10-24 上午11:31:39
  * @version 1.0.0
  */
-public class FixedHeaderFrameDecoderNetty4 extends ReplayingDecoder<Void> {
+public class CanalPacketDecoder extends ReplayingDecoder<Void> {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf byteBuf, List<Object> out)throws Exception {
-        out.add(byteBuf.readBytes(byteBuf.readInt()));
+        byte[] bytes = new byte[byteBuf.readInt()];
+        byteBuf.readBytes(bytes);
+        CanalPacket.Packet packet = CanalPacket.Packet.parseFrom(bytes);
+        out.add(packet);
     }
 }
--- server/src/main/java/com/alibaba/otter/canal/server/netty4/handler/ClientAuthenticationHandlerNetty4.java
+++ server/src/main/java/com/alibaba/otter/canal/server/netty4/handler/ClientAuthenticationHandlerNetty4.java
@@ -5,11 +5,19 @@ import com.alibaba.otter.canal.common.zookeeper.running.ServerRunningMonitors;
 import com.alibaba.otter.canal.protocol.CanalPacket;
 import com.alibaba.otter.canal.protocol.ClientIdentity;
 import com.alibaba.otter.canal.server.embedded.CanalServerWithEmbedded;
+import com.alibaba.otter.canal.server.netty.NettyUtils;
+import com.alibaba.otter.canal.server.netty.handler.ClientAuthenticationHandler;
+import com.alibaba.otter.canal.server.netty.handler.HandshakeInitializationHandler;
+import com.alibaba.otter.canal.server.netty.handler.SessionHandler;
 import com.alibaba.otter.canal.server.netty4.Netty4Utils;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
-import io.netty.util.AttributeKey;
 import org.apache.commons.lang.StringUtils;
+import org.jboss.netty.handler.timeout.IdleStateAwareChannelHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -22,54 +30,62 @@ import java.util.concurrent.TimeUnit;
  * @author jianghang 2012-10-24 上午11:12:45
  * @version 1.0.0
  */
-public class ClientAuthenticationHandlerNetty4 extends SimpleChannelInboundHandler<CanalPacket.ClientAuth> {
+public class ClientAuthenticationHandlerNetty4 extends SimpleChannelInboundHandler<CanalPacket.Packet> {
 
     private static final Logger logger = LoggerFactory.getLogger(ClientAuthenticationHandlerNetty4.class);
-    private final int defaultSubscriptorDisconnectIdleTimeout = 5 * 60 * 1000;
+    private final long defaultSubscriptorDisconnectIdleTimeout = 5 * 60 * 1000;
     private CanalServerWithEmbedded embeddedServer;
+    private final int SUPPORTED_VERSION = 3;
 
     public ClientAuthenticationHandlerNetty4(CanalServerWithEmbedded embeddedServer) {
         this.embeddedServer = embeddedServer;
     }
 
     @Override
-    public void channelRead0(final ChannelHandlerContext ctx, final CanalPacket.ClientAuth clientAuth) throws Exception{
-        logger.info("进入身份认证的Handler [username {}]----[password {}] ",clientAuth.getUsername(),clientAuth.getPassword().toStringUtf8());
-        Channel channel = ctx.channel();
-        if(StringUtils.isNotEmpty(clientAuth.getDestination()) && StringUtils.isNotEmpty(clientAuth.getClientId())){
-            ClientIdentity clientIdentity = new ClientIdentity(clientAuth.getDestination(), Short.valueOf(clientAuth.getClientId()), clientAuth.getFilter());
-            try{
-                MDC.put("destination", clientIdentity.getDestination());
-                embeddedServer.subscribe(clientIdentity);
-                channel.attr(AttributeKey.valueOf("clientIdentity")).set(clientIdentity);
-                // 尝试启动，如果已经启动，忽略
-                if(!embeddedServer.isStart(clientIdentity.getDestination())){
-                    ServerRunningMonitor runningMonitor = ServerRunningMonitors.getRunningMonitor(clientIdentity.getDestination());
-                    if(!runningMonitor.isStart()){
-                        runningMonitor.start();
+    protected void channelRead0(final ChannelHandlerContext ctx, CanalPacket.Packet packet) throws Exception {
+        switch (packet.getVersion()) {
+            case SUPPORTED_VERSION:
+            default:
+                final CanalPacket.ClientAuth clientAuth = CanalPacket.ClientAuth.parseFrom(packet.getBody());
+                logger.info("进入身份认证的Handler [username {}]----[password {}] ",clientAuth.getUsername(),clientAuth.getPassword().toStringUtf8());
+                // 如果存在订阅信息
+                if (StringUtils.isNotEmpty(clientAuth.getDestination()) && StringUtils.isNotEmpty(clientAuth.getClientId())) {
+                    ClientIdentity clientIdentity = new ClientIdentity(clientAuth.getDestination(),Short.valueOf(clientAuth.getClientId()),clientAuth.getFilter());
+                    try {
+                        MDC.put("destination", clientIdentity.getDestination());
+                        embeddedServer.subscribe(clientIdentity);
+                        // 尝试启动，如果已经启动，忽略
+                        if (!embeddedServer.isStart(clientIdentity.getDestination())) {
+                            ServerRunningMonitor runningMonitor = ServerRunningMonitors.getRunningMonitor(clientIdentity.getDestination());
+                            if (!runningMonitor.isStart()) {
+                                runningMonitor.start();
+                            }
+                        }
+                    } finally {
+                        MDC.remove("destination");
                     }
                 }
-            }finally{
-                MDC.remove("destination");
-            }
-        }
-        Netty4Utils.ack(channel,new ChannelFutureListener() {
-                    public void operationComplete(ChannelFuture future) throws Exception{
+                Netty4Utils.ack(ctx.channel(), new ChannelFutureListener() {
+                    public void operationComplete(ChannelFuture future) throws Exception {
                         logger.info("remove unused channel handlers after authentication is done successfully.");
                         ctx.pipeline().remove(HandshakeInitializationHandlerNetty4.class.getName());
                         ctx.pipeline().remove(ClientAuthenticationHandlerNetty4.class.getName());
-                        int readTimeout = defaultSubscriptorDisconnectIdleTimeout;
-                        int writeTimeout = defaultSubscriptorDisconnectIdleTimeout;
-                        if(clientAuth.getNetReadTimeout() > 0){
+
+                        long readTimeout = defaultSubscriptorDisconnectIdleTimeout;
+                        long writeTimeout = defaultSubscriptorDisconnectIdleTimeout;
+                        if (clientAuth.getNetReadTimeout() > 0) {
                             readTimeout = clientAuth.getNetReadTimeout();
                         }
-                        if(clientAuth.getNetWriteTimeout() > 0){
+                        if (clientAuth.getNetWriteTimeout() > 0) {
                             writeTimeout = clientAuth.getNetWriteTimeout();
                         }
                         IdleStateHandler idleStateHandler = new IdleStateHandler(readTimeout, writeTimeout, 0, TimeUnit.MILLISECONDS);
-                        ctx.pipeline().addBefore(SessionHandlerNetty4.class.getName(), IdleStateHandler.class.getName(), idleStateHandler);
+                        ctx.pipeline().addBefore(SessionHandlerNetty4.class.getName(),
+                                                 IdleStateHandler.class.getName(),idleStateHandler);
                     }
 
                 });
+                break;
+        }
     }
 }
