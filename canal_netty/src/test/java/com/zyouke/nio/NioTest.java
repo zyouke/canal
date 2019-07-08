8396bbee86af7419c148d728275a389c8b2abb39
zhoujun <zhoujun@okay.cn>
2019-07-08 18:41:13 +0800

    原生nio服务端开发

--- canal_netty/src/test/java/com/zyouke/nio/NioTest.java
+++ canal_netty/src/test/java/com/zyouke/nio/NioTest.java
@@ -19,7 +19,7 @@ public class NioTest {
             System.out.println(socketChannel.getRemoteAddress().toString() + "连接成功");
             for (int i = 0; i < 100; i++) {
                 ByteBuffer byteBuffer = ByteBuffer.allocate(20);
-                byteBuffer.put("zyouke".getBytes());
+                byteBuffer.put(("zyouke" + i).getBytes());
                 byteBuffer.flip();
                 socketChannel.write(byteBuffer);
             }
